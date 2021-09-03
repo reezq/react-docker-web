38563e922f3aafe20bd63de922477e6b1f5e061d
reezq <reezcliquers@gmail.com>
2021-09-03 15:20:59 +0700

    coba ganti app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ujicoba upload ke GCP
+          Winda Cantik
         </p>
         <a
           className="App-link"
